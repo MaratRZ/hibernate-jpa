25ac7fc03ef28eaa3698e1c6be1141ce462eed93
Marat Zalyaletdinov <marat.rz@gmail.com>
2021-12-27 22:27:51 +0300

    initial commit

---
+++ src/main/java/ru/gb/ShopApp.java
@@ -0,0 +1,62 @@
+package ru.gb;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.gb.config.JpaConfig;
+import ru.gb.dao.CartDao;
+import ru.gb.dao.ManufacturerDao;
+import ru.gb.dao.ProductDao;
+import ru.gb.entity.Cart;
+import ru.gb.entity.Manufacturer;
+import ru.gb.entity.Product;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class ShopApp {
+
+
+
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JpaConfig.class);
+        ManufacturerDao manufacturerDao = context.getBean(ManufacturerDao.class);
+//        ProductDao productDao = context.getBean(ProductDao.class);
+//        CartDao cartDao = context.getBean(CartDao.class);
+
+//        for (Product product : productDao.findAll()) {
+//            System.out.println(product);
+//        }
+
+
+//        System.out.println(manufacturerDao.findNameById(3L));
+//        System.out.println("-----------------------");
+        System.out.println(manufacturerDao.findById(4L).getProducts());
+        Manufacturer manufacturer = manufacturerDao.findById(4L);
+        // УДАЛЕНИЕ
+        manufacturerDao.delete(manufacturer);
+//        System.out.println("-----------------------");
+//        for (Manufacturer manufacturer : manufacturerDao.findAll()) {
+//            System.out.println(manufacturer);
+//        }
+
+//        Manufacturer testManufacturer = Manufacturer.builder()
+//                .name("Company 4")
+//                .products(new HashSet<Product>(((List<Product>) productDao.findAll()).subList(1, 5)))
+//                .build();
+//        System.out.println(testManufacturer);
+//
+//        manufacturerDao.save(testManufacturer);
+//        System.out.println(testManufacturer);
+//        Manufacturer savedManufacturer = manufacturerDao.findById(3L);
+//        savedManufacturer.setName("Apple");
+//        manufacturerDao.save(savedManufacturer);
+
+//        manufacturerDao.deleteById(3L);
+
+//        Product product = ((List<Product>) productDao.findAll()).get(5);
+//
+//        Cart cart = new Cart();
+//        cart.addProduct(product);
+//        cartDao.save(cart);
+    }
+}
